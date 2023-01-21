3a937214d24dd0a95d75b099409ca9f08b83411c
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2023-01-22 03:27:46 +0900

    初版

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# never-push-me
-絶対に押すなよボタン
+# 絶対に！押すなボタン
+
+![絶対に！押すな](images/never-push-me.jpg)
+
+絶対に押しちゃダメなボタンです。押すなよ，絶対に押すなよ。
+
+元ネタは「[『絶対に！押すな』のボタン、押し放題の誘惑](https://www.nikkei.com/article/DGXZQOUC26A8A0W2A420C2000000/)」（2022 年 6 月 5 日 日経 MJ）です。
+
+## 動作環境
+
+[M5Stack 社](https://m5stack.com/) の [ATOMS3](https://shop.m5stack.com/products/atoms3-dev-kit-w-0-85-inch-screen) で[M5Unified](https://github.com/m5stack/M5Unified)を使って実装しています。[VSCode](https://azure.microsoft.com/ja-jp/products/visual-studio-code) の [PlatformIO IDE](https://platformio.org/platformio-ide) 環境でコンパイルしてください。
+
+私は Windows 11 上で動作確認をしています。
+
+## 実行方法
+
+ATOMS3 を PC に接続すると，USB キーボードとして認識されます。上記の写真では，[スイッチサイエンス](https://www.switch-science.com/)で販売している[USB Type-A/Type-C プラグアダプタ](https://ssci.to/7998)を使用しています。
+
+絶対に押しちゃダメですが，押すと`CTRL+ALT+DELETE`が押されたのと同じ動作になります。
---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <M5Unified.h>
+#include <USB.h>
+#include <USBHIDKeyboard.h>
+
+const uint32_t BACKGROUND_COLOR = M5.Lcd.color888(255, 25, 25);
+
+const struct
+{
+    const char* label;
+    int color;
+    int32_t x;
+    int32_t y;
+    const lgfx::IFont* font;
+} LABELS[] = {
+    {"絶対に！", TFT_WHITE, 64, 30, &fonts::lgfxJapanGothic_16},
+    {"押すな", TFT_WHITE, 64, 50, &fonts::lgfxJapanGothic_28},
+};
+const size_t N_LABELS = sizeof(LABELS) / sizeof(LABELS[0]);
+
+const uint8_t KEYS[] = {
+    KEY_LEFT_CTRL,
+    KEY_LEFT_ALT,
+    KEY_DELETE,
+};
+const size_t N_KEYS = sizeof(KEYS) / sizeof(KEYS[0]);
+
+USBHIDKeyboard keyboard;
+
+void setup(void) {
+    M5.begin();
+    M5.Lcd.setRotation(3);  // 左側にUSB-Cの口が来る向き
+    M5.Lcd.startWrite();
+    M5.Lcd.clear(BACKGROUND_COLOR);
+    for (size_t i = 0; i < N_LABELS; ++i) {
+        M5.Lcd.setTextColor(LABELS[i].color, BACKGROUND_COLOR);
+        M5.Lcd.drawCenterString(LABELS[i].label, LABELS[i].x, LABELS[i].y,
+                                LABELS[i].font);
+    }
+    M5.Lcd.endWrite();
+
+    keyboard.begin();
+    USB.begin();
+}
+
+void loop(void) {
+    vTaskDelay(1);
+    M5.update();
+    if (M5.BtnA.wasClicked()) {
+        for (size_t i = 0; i < N_KEYS; ++i) {
+            keyboard.press(KEYS[i]);
+        }
+    } else {
+        keyboard.releaseAll();
+    }
+}
